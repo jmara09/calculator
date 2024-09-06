4eefae1f1917b4d6f3d46e95e2bc5713001347bc
Jmara <jmara.0923@gmail.com>
2024-09-06 22:28:38 +0200

    Make equal sign show result in display

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const display = document.querySelector(".display");
 let operandOne = "";
 let operandTwo = "";
 let mathOperator = "";
+let sum = 0;
 
 function inputNum(e) {
   if (e.target.id === "allclear") {
@@ -11,7 +12,13 @@ function inputNum(e) {
     mathOperator = "";
     display.textContent = "";
   }
-  if (e.target.className == "operator" && operandOne == "";)
+
+  if (e.target.className == "operator arth" && operandOne == "") {
+    return;
+  } else if (e.target.className == "operator arth") {
+    mathOperator = e.target.id;
+  }
+
   if (operandOne.length == 10) return;
   if (mathOperator == "") {
     if (e.target.className === "number btn") {
@@ -22,20 +29,35 @@ function inputNum(e) {
     // operandOne = operandOne.split("");
     // operandOne.splice(1, 1);
     // console.log(operandOne);
-  } else if (mathOperator != "")
+  } else if (mathOperator != "") {
+    if (e.target.className === "number btn") {
+      operandTwo += e.target.innerText;
+      display.textContent = `${operandTwo}`;
+    }
+  }
 }
 
 button.addEventListener("click", inputNum);
 
 function operate(operator, num1, num2) {
   switch (operator) {
-    case "+":
-      return num1 + num2;
-    case "-":
-      return num1 - num2;
-    case "*":
-      return num1 * num2;
-    case "/":
-      return num1 / num2;
+    case "add":
+      sum = parseInt(num1) + parseInt(num2);
+      break;
+    case "minus":
+      sum = parseInt(num1) - parseInt(num2);
+      break;
+    case "multiply":
+      sum = parseInt(num1) * parseInt(num2);
+      break;
+    case "division":
+      sum = parseInt(num1) / parseInt(num2);
   }
 }
+
+button.addEventListener("click", (e) => {
+  if (e.target.id == "equals") {
+    operate(mathOperator, operandOne, operandTwo);
+    display.textContent = sum;
+  }
+});

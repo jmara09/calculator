ff4c9097046d958e4f1397f32da11de96e472617
Jmara <jmara.0923@gmail.com>
2024-09-06 21:37:45 +0200

    Make function for clear button

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 const button = document.querySelector(".numbers");
 const display = document.querySelector(".display");
-let operandOne = 0;
-let operandTwo = 0;
-let mathOperator;
+let operandOne = "";
+let operandTwo = "";
+let mathOperator = "";
 
-button.addEventListener("click", (e) => {
-  if (e.target.className === "number btn") {
-    console.log(e.target.className);
-    operandOne = e.target.innerText;
-    display.textContent = `${operandOne}`;
-    console.log(operandOne);
+function inputNum(e) {
+  if (e.target.id === "allclear") {
+    operandOne = "";
+    operandTwo = "";
+    mathOperator = "";
+    display.textContent = "";
   }
-});
+  if (e.target.className == "operator" && operandOne == "";)
+  if (operandOne.length == 10) return;
+  if (mathOperator == "") {
+    if (e.target.className === "number btn") {
+      operandOne += e.target.innerText;
+      display.textContent = `${operandOne}`;
+    }
+    // console.log(operandOne);
+    // operandOne = operandOne.split("");
+    // operandOne.splice(1, 1);
+    // console.log(operandOne);
+  } else if (mathOperator != "")
+}
+
+button.addEventListener("click", inputNum);
 
 function operate(operator, num1, num2) {
   switch (operator) {

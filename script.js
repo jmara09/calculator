4b3b71f50e32b514829a24bfd586c4b0f1783241
Jmara <jmara.0923@gmail.com>
2024-09-07 15:39:51 +0200

    Separate functions of buttons for readability

--- script.js
+++ script.js
@@ -5,59 +5,149 @@ let operandTwo = "";
 let mathOperator = "";
 let sum = 0;
 
-function inputNum(e) {
-  if (e.target.id === "allclear") {
-    operandOne = "";
-    operandTwo = "";
-    mathOperator = "";
-    display.textContent = "";
-  }
+function clearAll() {
+  operandOne = "";
+  operandTwo = "";
+  mathOperator = "";
+  display.textContent = "";
+}
 
+function percentage() {
+  if (display.className === "display one") {
+    operandOne /= 100;
+    display.textContent = operandOne;
+  } else {
+    operandTwo /= 100;
+    display.textContent = operandTwo;
+  }
+}
+
+function changeSign() {
+  if (display.className === "display one") {
+    operandOne = operandOne.split("");
+    if (!operandOne.includes("-")) {
+      operandOne.splice(0, 0, "-");
+      operandOne = operandOne.join("");
+      display.textContent = operandOne;
+    } else {
+      operandOne.splice(0, 1);
+      operandOne = operandOne.join("");
+      display.textContent = operandOne;
+    }
+  } else {
+    operandTwo = operandTwo.split("");
+    if (!operandTwo.includes("-")) {
+      operandTwo.splice(0, 0, "-");
+      operandTwo = operandTwo.join("");
+      display.textContent = operandTwo;
+    } else {
+      operandTwo.splice(0, 1);
+      operandTwo = operandTwo.join("");
+      display.textContent = operandTwo;
+    }
+  }
+}
+
+function backspace() {
+  if (display.className === "display one") {
+    operandOne = operandOne.split("");
+    operandOne.splice(operandOne.length - 1, 1);
+    operandOne = operandOne.join("");
+    display.textContent = operandOne;
+  } else {
+    operandTwo = operandTwo.split("");
+    operandTwo.splice(operandTwo.length - 1, 1);
+    operandTwo = operandTwo.join("");
+    display.textContent = operandTwo;
+  }
+}
+
+function decimal() {
+  if (display.className === "display one") {
+    operandOne = operandOne.split("");
+    if (!operandOne.includes(".")) {
+      operandOne.splice(operandOne.length, 0, ".");
+      operandOne = operandOne.join("");
+      display.textContent = operandOne;
+    } else {
+      operandOne = operandOne.join("");
+    }
+  } else {
+    operandTwo = operandTwo.split("");
+    if (!operandTwo.includes(".")) {
+      operandTwo.splice(operandTwo.length, 0, ".");
+      operandTwo = operandTwo.join("");
+      display.textContent = operandTwo;
+    } else {
+      operandTwo = operandTwo.join("");
+    }
+  }
+}
+
+function userInput(e) {
   if (e.target.className == "operator arth" && operandOne == "") {
     return;
   } else if (e.target.className == "operator arth") {
     mathOperator = e.target.id;
   }
 
-  if (operandOne.length == 10) return;
   if (mathOperator == "") {
-    if (e.target.className === "number btn") {
+    if (e.target.className === "number btn" && operandOne.length != 10) {
+      display.classList.add("one");
       operandOne += e.target.innerText;
       display.textContent = `${operandOne}`;
     }
-    // console.log(operandOne);
-    // operandOne = operandOne.split("");
-    // operandOne.splice(1, 1);
-    // console.log(operandOne);
   } else if (mathOperator != "") {
-    if (e.target.className === "number btn") {
+    if (e.target.className === "number btn" && operandTwo.length != 10) {
+      display.classList.remove("one");
       operandTwo += e.target.innerText;
       display.textContent = `${operandTwo}`;
     }
   }
 }
 
-button.addEventListener("click", inputNum);
+button.addEventListener("click", (e) => {
+  if (e.target.id === "allclear") clearAll();
+  if (e.target.id === "percent") percentage();
+  if (e.target.id === "sign") changeSign();
+  if (e.target.id === "backspace") backspace();
+  if (e.target.id === "decim") decimal();
+});
+
+button.addEventListener("click", userInput);
 
 function operate(operator, num1, num2) {
   switch (operator) {
     case "add":
-      sum = parseInt(num1) + parseInt(num2);
+      sum = parseFloat(num1) + parseFloat(num2);
       break;
     case "minus":
-      sum = parseInt(num1) - parseInt(num2);
+      sum = parseFloat(num1) - parseFloat(num2);
       break;
     case "multiply":
-      sum = parseInt(num1) * parseInt(num2);
+      sum = parseFloat(num1) * parseFloat(num2);
       break;
     case "division":
-      sum = parseInt(num1) / parseInt(num2);
+      sum = num2 == 0 ? "lull" : parseFloat(num1) / parseFloat(num2);
   }
 }
 
 button.addEventListener("click", (e) => {
   if (e.target.id == "equals") {
     operate(mathOperator, operandOne, operandTwo);
-    display.textContent = sum;
+    sum = sum.toString();
+    checkSumDecimal = sum.split("");
+    if (sum.length > 10) {
+      if (checkSumDecimal.includes(".")) {
+        sum = parseFloat(checkSumDecimal.join(""));
+        display.textContent = sum.toFixed(8);
+      } else {
+        sum = parseFloat(sum);
+        sum = sum.toExponential(4);
+        display.textContent = sum;
+      }
+    } else {
+      display.textContent = sum;
+    }
   }
 });
